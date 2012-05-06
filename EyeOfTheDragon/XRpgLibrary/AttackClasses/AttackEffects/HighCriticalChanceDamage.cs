86670433cd9c9831675ee2bcc3744bdfc1f1b6b3
Turak.Katase@gmail.com <Turak.Katase@gmail.com@376e02cd-2745-4999-ea55-181a1a95918d>
2012-05-06 14:21:42 +0000

    Added new attack effect. Removed more build folders.

---
+++ EyeOfTheDragon/XRpgLibrary/AttackClasses/AttackEffects/HighCriticalChanceDamage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using XRpgLibrary.PokemonClasses;
+
+namespace XRpgLibrary.AttackClasses.AttackEffects
+{
+    public class HighCriticalChanceDamage : AttackEffect
+    {
+        #region Protected Members
+        protected int power;
+        #endregion Protected Members
+
+        #region Constructors
+        public HighCriticalChanceDamage(int damage, float effectPercentage)
+            : base(effectPercentage)
+        {
+            power = damage;
+        }
+
+        public HighCriticalChanceDamage()
+            : base(1f)
+        {
+            power = 0;
+        }
+        #endregion Constructors
+
+        #region Public Attributes
+        /// <summary>
+        /// Gets or sets the power.
+        /// </summary>
+        public int Power
+        {
+            get { return power; }
+            set { power = value; }
+        }
+        #endregion Public Attributes
+
+        public override void ApplyEffect(Pokemon user, Pokemon target, Attack attack)
+        {
+            user.modifyStatStage(Stat.Critical, 1);
+            target.damage(BattleCalculator.CalculateDamage(user, target, attack, power));
+            user.modifyStatStage(Stat.Critical, -1);
+        }
+
+        public override string ToString()
+        {
+            string toString = "High Critical Chance Damage Damage";
+            toString += ", " + power;
+            toString += ", " + effectPercentage;
+
+            return toString;
+        }
+    }
+}
